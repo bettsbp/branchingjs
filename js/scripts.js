64ff2b446ee4d3aedad2f243861cb62c5a5f0a6d
Brandon Betts and Craig Wann <craig@>
2018-05-31 11:58:44 -0700

    add add new scripts for voting, add new html pages

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,22 @@ $(document).ready(function() {
 
   if (age > 21) {
     $('#drinks').show();
+    $('.well').show();
   } else if (age === 21) {
     alert("Now don't go crazy!");
     $('#drinks').show();
+    $('.well').show();
   } else {
     $('#under-21').show();
-  }
+    $('.well').show();
+  };
+
+  if (age > 18) {
+    $('#voting').show();
+    $('.well').show();
+  } else {
+    $('#under-18').show();
+    $('.well').show();
+  };
+
 });
